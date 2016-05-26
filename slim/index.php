c33ed41c0d502fc27e7584aa969e12bb8521406d
Toshiki Inami <09c0507@gmail.com>
2016-05-26 10:52:59 +0900

    Add /n explicitly

--- slim/index.php
+++ slim/index.php
@@ -7,7 +7,7 @@ require 'vendor/autoload.php';
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
       $name = $request->getAttribute('name');
-          $response->getBody()->write("Hello, $name");
+          $response->getBody()->write("Hello, $name\n");
 
           return $response;
 });
